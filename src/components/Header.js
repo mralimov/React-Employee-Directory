664482e696d20c6b5ba141fb0c81f6dd58ab3209
mralimov <mralimov89@gmail.com>
2020-10-19 16:18:01 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,7 @@ function Header() {
   return (
     <div className='header'>
       <h1>Employee Directory</h1>
-      <p>
-        Click on carrots to filter by heading or use the search box to narrow
-        your results.
-      </p>
+      <p>Search for Employee Directory by inputting thier name</p>
     </div>
   );
 }
